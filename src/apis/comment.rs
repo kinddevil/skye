4343e821966ead071b4a7a6eec8313206f476e5f
yichen <yichenwei@acm.org>
2021-03-14 11:51:44 -0700

    init comment

---
+++ src/apis/comment.rs
@@ -0,0 +1,27 @@
+use actix_http::http::StatusCode;
+
+use actix_web::{web, HttpRequest, HttpResponse};
+use log::info;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Comment {
+    comment: String,
+    author: String,
+    timestamp: String,
+}
+
+pub async fn comment(info: web::Json<Comment>, _req: HttpRequest) -> HttpResponse {
+    info!("{:?}", info);
+    HttpResponse::build(StatusCode::OK).body("ok")
+}
+
+#[cfg(test)]
+mod login_tests {
+    use super::*;
+    use actix_web::{test, web::Bytes, App};
+
+    #[actix_rt::test]
+    #[ignore]
+    async fn test_wx_login() {}
+}
